1c28f3a29da65ac079b46465318735e9fdb9536a
thesliz3r <104354951+thesliz3r@users.noreply.github.com>
2022-07-10 19:00:26 +0400

    AUE_TRACKER

---
+++ mainone.py
@@ -0,0 +1,34 @@
+import phonenumbers
+import folium
+
+import opencage
+
+from myphones import number
+
+from phonenumbers import geocoder
+
+pepnumber = phonenumbers.parse(number)
+location = geocoder.description_for_number(pepnumber, 'en')
+print(location)
+
+from phonenumbers import carrier
+service_pro = phonenumbers.parse(number)
+print(carrier.name_for_number(service_pro, 'en'))
+
+from opencage.geocoder import OpenCageGeocode
+key = '5f124cccb75242238034b97a3b5a6ecd'
+geocoder = OpenCageGeocode(key)
+query = str(location)
+results = geocoder.geocode(query)
+print(results)
+
+lat = results[0]['geometry']['lat']
+lng = results[0]['geometry']['lng']
+
+print(lng, lng)
+
+myMap = folium.Map(location=[lat, lng], zoom_start=9)
+folium.Marker([lat, lng], popup=location).add_to(myMap)
+
+myMap.save("mylocation.html")
+
